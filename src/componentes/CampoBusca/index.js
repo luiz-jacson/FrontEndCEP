dc017f029023d23e443a0a9e204cb9f00700e99d
Luiz Jacson <luiz.jacson@souunit.com.br>
2022-09-06 13:38:34 -0300

    adicionando loader spinner

--- src/componentes/CampoBusca/index.js
+++ src/componentes/CampoBusca/index.js
@@ -1,22 +1,29 @@
 import { useState } from 'react';
 import './CampoBusca.css';
 import CardEndereco from '../CardEndereco';
+import { MagnifyingGlass } from 'react-loader-spinner';
+
 
 const CampoBusca = (props) => {
 
     const [valor, setValor] = useState('');
     const [end, setEnd] = useState([]);
+    const [carregando, setCarregando] = useState(false);
     const Busca = (evento) => {
+        setCarregando(true);
         evento.preventDefault();
-        fetch('https://localhost:7185/CEP?valor=' + valor)
+        fetch('http://localhost:3001/?valor=' + valor)
             .then(response => response.json())
             .then(json => {
                 setEnd(json);
+                setCarregando(false)
             })
             .catch(erro => {
                 console.log(erro);
+                setCarregando(false)
             }
             )
+        
     }
     const setEvento = (evento) => {
         setValor(evento.target.value);
@@ -28,9 +35,19 @@ const CampoBusca = (props) => {
                 <h1>Buscar Endereço</h1>
                 <input value={valor} onChange={setEvento} placeholder={props.placeholder}></input>
                 <button>Buscar</button>
-                {end.status == 200? 
-                <CardEndereco endereco = {end}/> : <h1 className='naoencontrado'>{end.mensagem}</h1>
-                }
+                {carregando === true?<MagnifyingGlass
+                                    wrapperClassName = "lupa"
+                                    visible={true}
+                                    ariaLabel="MagnifyingGlass-loading"
+                                    wrapperStyle={{}}
+                                    wrapperClass="MagnifyingGlass-wrapper"
+                                    glassColor = '#c0efff'
+                                    color = '#e15b64'
+                />:end.status === 200? 
+                <CardEndereco endereco = {end}/> : <h1 className='naoencontrado'>{end.mensagem}</h1>}
+                
+                
+                
                 
             </form>
         </div>
